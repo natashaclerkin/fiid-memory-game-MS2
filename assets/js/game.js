794c7ecb5e8ba01b65616a79e997ccf69af35831
natashaclerkin <natashaclerkin@gmail.com>
2021-02-01 21:03:05 +0000

    Update time for testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -152,7 +152,7 @@ class MixOrMatch {
 function ready() {
   let overlays = Array.from(document.getElementsByClassName('overlay-text'));
   let cards = Array.from(document.getElementsByClassName('card'));
-  let game = new MixOrMatch(120, cards);
+  let game = new MixOrMatch(60, cards);
 
   overlays.forEach(overlay => {
     overlay.addEventListener('click', () => {
